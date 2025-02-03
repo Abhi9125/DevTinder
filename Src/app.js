4f3c165c79876b08e29af4dc19b5953557ca330a
Abhi9125 <abhi.singh3132@gmail.com>
2025-02-03 11:35:17 +0530

    Read document form the database using get methods

--- Src/app.js
+++ Src/app.js
@@ -10,7 +10,71 @@ const User = require("./models/user");
 // Middleware to parse the json to js object
 app.use(express.json());
 
-// Route to handle user signup and save user data to the database
+/**
+ * Route to retrieve a user by their email.
+ * Expects 'emailId' in the request body.
+ */
+app.get("/user", async (req, res) => {
+  // Extracting the emailId from the request body
+  const userEmail = req.body.emailId;
+  // console.log(userEmail);
+  try {
+    const user = await User.find({ email: userEmail });
+    res.send(user);
+  } catch {
+    res.status(404).send("User not found");
+  }
+});
+
+/**
+ * Route to retrieve all users from the database.
+ */
+app.get("/feed", async (req, res) => {
+  try {
+    // Fetching all users
+    const alluser = await User.find({});
+
+    res.send(alluser);
+  } catch {
+    res.status(404).send("User not found");
+  }
+});
+
+/**
+ * Route to retrieve a user by their unique ID.
+ * Expects 'id' in the request body.
+ */
+app.get("/id", async (req, res) => {
+  // Extracting the Id from the request body
+  const id = req.body.id;
+  try {
+    const userById = await User.findById(id);
+
+    res.send(userById);
+  } catch {
+    res.status(404).send("User not found");
+  }
+});
+
+/**
+ * Route to retrieve a single user by their email.
+ * Expects 'emailId' in the request body.
+ */
+app.get("/findone", async (req, res) => {
+  const emails = req.body.emailId;
+  try {
+    const userById = await User.findOne({ email: emails });
+
+    res.send(userById);
+  } catch {
+    res.status(404).send("User not found");
+  }
+});
+
+/**
+ * Route to handle user signup and save user data to the database.
+ * Expects user details in the request body.
+ */
 app.post("/signup", async (req, res) => {
   // Creating a new user instance with sample data
 
